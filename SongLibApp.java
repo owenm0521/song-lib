fe706bbcefa2df7a7d2868fc5facd3278448de4a
Owen Morris <69168904+owenm0521@users.noreply.github.com>
2021-09-27 11:01:04 -0400

    Add files via upload

--- SongLibApp.java
+++ SongLibApp.java
@@ -1,18 +1,31 @@
 package songlib.app;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class SongLibApp extends Application {
 
 	@Override
-	public void start(Stage arg0) throws Exception {
+	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("/songlib/view/songlib.fxml"));
 
+		AnchorPane root = (AnchorPane)loader.load();
+
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("My Library");
+		primaryStage.setResizable(false);
+		primaryStage.show();
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		launch(args);
 
 	}
 
